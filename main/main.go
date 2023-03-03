88dab8f414da816e47075e4876f35041e2ee28c1
WangZY <76583642+xmx@users.noreply.github.com>
2023-03-03 16:00:49 +0800

    go get -u

--- main/main.go
+++ main/main.go
@@ -6,6 +6,7 @@ import (
 	"os/signal"
 	"syscall"
 
+	"github.com/vela-ssoc/backend-common/encipher"
 	"github.com/vela-ssoc/backend-common/logback"
 	"github.com/vela-ssoc/vela-minion/banner"
 	"github.com/vela-ssoc/vela-minion/launch"
@@ -16,14 +17,10 @@ func main() {
 	banner.Print(os.Stdout)
 	slog := logback.Stdout()
 
-	addr := tunnel.Addresses{
-		{TLS: true, Addr: "172.31.61.168", Name: "local.eastmoney.com"},
-		{Addr: "172.31.61.168:8180"},
-	}
-	hide := tunnel.Hide{
-		Semver:   "0.0.1-delve",
-		Ethernet: addr,
-		Internet: nil,
+	var hide tunnel.Hide
+	if err := encipher.ReadFile(os.Args[0], &hide); err != nil {
+		slog.Errorf("读取 hide 数据错误：%v", err)
+		return
 	}
 
 	// 监听停止信号
@@ -33,5 +30,5 @@ func main() {
 	slog.Info("按 Ctrl+C 结束运行")
 
 	err := launch.Run(ctx, hide, slog)
-	slog.Warnf("程序已结束运行：%v", err)
+	slog.Warn("程序已结束运行：%v", err)
 }
